64e6bec0ac87c92b6d47588b1446a82471627074
linhui <linhui0705@gmail.com>
2021-11-13 11:52:06 +0800

    style: 层次性依赖查找

--- spring-beans/src/main/java/org/springframework/beans/factory/BeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/BeanFactory.java
@@ -240,15 +240,20 @@ public interface BeanFactory {
 	/**
 	 * Does this bean factory contain a bean definition or externally registered singleton
 	 * instance with the given name?
+	 * 这个 bean 工厂是否包含具有给定名称的 bean 定义或外部注册的单例实例？
 	 * <p>If the given name is an alias, it will be translated back to the corresponding
 	 * canonical bean name.
+	 * 如果给定的名称是别名，它将被转换回相应的规范 bean 名称。
 	 * <p>If this factory is hierarchical, will ask any parent factory if the bean cannot
 	 * be found in this factory instance.
+	 * 如果这个工厂是分层的，如果在这个工厂实例中找不到 bean，将询问任何父工厂。
 	 * <p>If a bean definition or singleton instance matching the given name is found,
 	 * this method will return {@code true} whether the named bean definition is concrete
 	 * or abstract, lazy or eager, in scope or not. Therefore, note that a {@code true}
 	 * return value from this method does not necessarily indicate that {@link #getBean}
 	 * will be able to obtain an instance for the same name.
+	 * 如果找到与给定名称匹配的 bean 定义或单例实例，则无论指定的 bean 定义是具体的还是抽象的、惰性的还是急切的、范围内的，此方法都将返回true 。
+	 * 因此，请注意，此方法的true返回值并不一定表示getBean将能够获得同名的实例。
 	 * @param name the name of the bean to query
 	 * @return whether a bean with the given name is present
 	 */
--- spring-beans/src/main/java/org/springframework/beans/factory/HierarchicalBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/HierarchicalBeanFactory.java
@@ -21,10 +21,12 @@ import org.springframework.lang.Nullable;
 /**
  * Sub-interface implemented by bean factories that can be part
  * of a hierarchy.
+ * 由 bean 工厂实现的子接口，可以是层次结构的一部分。
  *
  * <p>The corresponding {@code setParentBeanFactory} method for bean
  * factories that allow setting the parent in a configurable
  * fashion can be found in the ConfigurableBeanFactory interface.
+ * 可以在 ConfigurableBeanFactory 接口中找到允许以可配置方式设置父级的 bean 工厂的相应setParentBeanFactory方法。
  *
  * @author Rod Johnson
  * @author Juergen Hoeller
@@ -35,6 +37,7 @@ public interface HierarchicalBeanFactory extends BeanFactory {
 
 	/**
 	 * Return the parent bean factory, or {@code null} if there is none.
+	 * 返回父 bean 工厂，如果没有，则返回null 。
 	 */
 	@Nullable
 	BeanFactory getParentBeanFactory();
@@ -42,8 +45,10 @@ public interface HierarchicalBeanFactory extends BeanFactory {
 	/**
 	 * Return whether the local bean factory contains a bean of the given name,
 	 * ignoring beans defined in ancestor contexts.
+	 * 返回本地 bean 工厂是否包含给定名称的 bean，忽略在祖先上下文中定义的 bean。
 	 * <p>This is an alternative to {@code containsBean}, ignoring a bean
 	 * of the given name from an ancestor bean factory.
+	 * 这是containsBean的替代方法，忽略来自祖先 bean 工厂的给定名称的 bean。
 	 * @param name the name of the bean to query
 	 * @return whether a bean with the given name is defined in the local factory
 	 * @see BeanFactory#containsBean
